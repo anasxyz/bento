fc7bd79b081522a56235374f3772b4c83451505b
anasxyz <anaswessam176@gmail.com>
2026-03-13 04:53:15 +0000

    container element

---
+++ src/element/container.rs
@@ -0,0 +1,56 @@
+use crate::element::element::Element;
+use crate::element::handle::Handle;
+use crate::element::layout::Layout;
+use crate::element::values::FlexDirection;
+use crate::fonts::Fonts;
+use std::any::Any;
+
+pub struct Container {
+    pub layout: Layout,
+    pub children: Vec<Handle>,
+}
+
+impl Container {
+    pub fn new() -> Self {
+        Self {
+            layout: Layout::default(),
+            children: Vec::new(),
+        }
+    }
+}
+
+impl Element for Container {
+    fn layout(&self) -> &Layout {
+        &self.layout
+    }
+    fn layout_mut(&mut self) -> &mut Layout {
+        &mut self.layout
+    }
+    fn measure(&self, _fonts: &mut Fonts, _max_width: Option<f32>) -> Option<(f32, f32)> {
+        None
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+}
+
+pub struct Row;
+impl Row {
+    pub fn new() -> Container {
+        let mut c = Container::new();
+        c.layout.flex_direction = FlexDirection::Row;
+        c
+    }
+}
+
+pub struct Column;
+impl Column {
+    pub fn new() -> Container {
+        let mut c = Container::new();
+        c.layout.flex_direction = FlexDirection::Col;
+        c
+    }
+}
