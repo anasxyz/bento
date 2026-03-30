2c08e5e6649e0a51d5e4f3f945b5757623a20f7b
anasxyz <anaswessam176@gmail.com>
2026-03-31 00:01:10 +0100

    bento_core: fix 1 pixel bleeding artifact

--- bento_wgpu/src/pipelines/rect.rs
+++ bento_wgpu/src/pipelines/rect.rs
@@ -194,7 +194,8 @@ impl RectPipeline {
             ],
             None => [0.0; 4],
         };
-        let aa = if pw <= 2.0 { 0.0 } else { 1.0 };
+        // fix 1 pixel bleeding
+        let aa = if scale > 1.0 && pw > 2.0 { 1.0 } else { 0.0 };
         let new_inst = Instance {
             pos_size: [px, py, pw, ph],
             params: [radius, aa, 0.0, 0.0],
