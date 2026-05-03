ebd06774e4662e2cf0014105ab85f4ce85659f88
anasxyz <anaswessam176@gmail.com>
2026-05-03 15:37:57 +0100

    [bento_wgpu] create gpu screen buffer

--- bento_wgpu/examples/basic.rs
+++ bento_wgpu/examples/basic.rs
@@ -1,3 +1,4 @@
+use bento_wgpu::RenderContext;
 use cosmic_text::FontSystem;
 use std::sync::Arc;
 use std::time::Instant;
@@ -9,7 +10,10 @@ use winit::{
 };
 
 struct App {
+    ctx: RenderContext,
     window: Option<Arc<Window>>,
+    surface: Option<bento_wgpu::Surface<'static>>,
+    renderer: Option<bento_wgpu::Renderer>,
 }
 
 impl ApplicationHandler for App {
@@ -18,7 +22,7 @@ impl ApplicationHandler for App {
             event_loop
                 .create_window(
                     Window::default_attributes()
-                        .with_title("rotation quality test")
+                        .with_title("demo")
                         .with_inner_size(winit::dpi::LogicalSize::new(800u32, 600u32)),
                 )
                 .unwrap(),
@@ -28,12 +32,24 @@ impl ApplicationHandler for App {
         let scale = window.scale_factor() as f32;
         let w = size.width as f32 / scale;
         let h = size.height as f32 / scale;
+
+        let surface = bento_wgpu::Surface::new(&self.ctx, Arc::clone(&window), w, h, scale);
+        let renderer = bento_wgpu::Renderer::new(&self.ctx, &surface);
+        self.window = Some(window);
+        self.surface = Some(surface);
+        self.renderer = Some(renderer);
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::RedrawRequested => {
-
+                let (Some(renderer), Some(surface)) =
+                    (self.renderer.as_mut(), self.surface.as_mut())
+                else {
+                    return;
+                };
+                renderer.render(&mut self.ctx, surface, [0.1, 0.1, 0.1, 1.0]);
+                self.window.as_ref().unwrap().request_redraw();
             }
 
             WindowEvent::Resized(_) | WindowEvent::ScaleFactorChanged { .. } => {
@@ -56,7 +72,10 @@ fn main() {
     let event_loop = EventLoop::new().unwrap();
     event_loop
         .run_app(&mut App {
+            ctx: ctx,
             window: None,
+            surface: None,
+            renderer: None,
         })
         .unwrap();
 }
--- bento_wgpu/src/pipelines/rect.rs
+++ bento_wgpu/src/pipelines/rect.rs
@@ -1,11 +1,11 @@
-use wgpu;
 use bytemuck::{Pod, Zeroable};
+use wgpu;
 
 #[repr(C)]
 #[derive(Copy, Clone, Pod, Zeroable)]
 pub struct RectInstance {
-    pub pos_size: [f32; 4];
-    pub color: [f32; 4]
+    pub pos_size: [f32; 4],
+    pub color: [f32; 4],
 }
 
 pub struct RectPipeline {
@@ -16,3 +16,45 @@ pub struct RectPipeline {
     capacity: usize,
 }
 
+impl RectPipeline {
+    pub fn new(
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        format: wgpu::TextureFormat,
+        screen_w: f32,
+        screen_h: f32,
+    ) -> Self {
+        // create rect shader module
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("rect shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("rect.wgsl").into()),
+        });
+
+        /*
+         * create buffer holding screen size floats, 4 bytes each
+         * shader needs to read it to know screen size to convert pixel
+         * coords to NDC
+         *
+         * UNIFORM = shader can read this buffer
+         * COPY_DST = CPU can write to this buffer
+         */
+        let screen_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("rect screen uniform"),
+            size: 8,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        /*
+         * convert two screen size floats into raw bytes &[u8]
+         * then upload from CPU memory to GPU buffer created above
+         *
+         * 0 is byte offset to start writing at
+         */
+        queue.write_buffer(
+            &screen_buffer,
+            0,
+            bytemuck::cast_slice(&[screen_w, screen_h]),
+        );
+    }
+}
