57d7e0feb6bab701daefc85e6a3c63f52d6d5455
anasxyz <anaswessam176@gmail.com>
2026-05-08 03:46:10 +0100

    update demo ex1

--- crates/bento/examples/ex1.rs
+++ crates/bento/examples/ex1.rs
@@ -4,7 +4,11 @@ fn main() {
     let mut app = App::new();
     let mut ui = Ui::new();
 
-    let button_handle = ui.add(Button::new("Click me", 100.0, 100.0));
+    let rect = ui.add(Rect::new(0.0, 0.0, 400.0, 100.0));
+    ui.get_mut(rect).unwrap().border_widths = [3.0; 4];
+    ui.get_mut(rect).unwrap().border_color = [0.0, 0.0, 0.0, 1.0];
+    ui.get_mut(rect).unwrap().color = [0.0, 1.0, 0.0, 0.1];
+    ui.get_mut(rect).unwrap().radii = [15.0; 4];
 
     app.open_window(WindowConfig::default(), ui);
     app.run();
