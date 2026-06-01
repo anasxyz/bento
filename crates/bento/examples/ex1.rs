08e585fd904a80c9a9b4e1ea04ed64ea2fa29b36
anasxyz <anaswessam176@gmail.com>
2026-06-01 06:03:01 +0100

    update ex1 with layout test

--- crates/bento/examples/ex1.rs
+++ crates/bento/examples/ex1.rs
@@ -1,4 +1,5 @@
 use bento::*;
+use std::time::Duration;
 
 fn main() {
     let mut app = App::new();
@@ -6,22 +7,33 @@ fn main() {
 
     let root = ui.root();
     ui.set(root, |g: &mut Group| {
-        g.layout = Layout::Column {
+        g.layout = Layout::Row {
             gap: 8.0,
             padding: [16.0, 16.0, 16.0, 16.0],
-            main_axis: MainAxis::Center,
-            cross_axis: CrossAxis::End,
+            main_axis: MainAxis::Start,
+            cross_axis: CrossAxis::Start,
             wrap: true,
         };
         g.width = Size::Fill;
         g.height = Size::Fill;
     });
 
-    let button = ui.add(root, Button::new(&format!("Button")));
-    ui.set(button, |b: &mut Button| {
-        b.set_border_radius([8.0, 8.0, 8.0, 8.0]);
-        b.set_border_color([0.0, 0.0, 0.0, 1.0]);
-        b.set_border_width([3.0, 3.0, 0.0, 0.0]);
+    let mut middle_btn = None;
+
+    for i in 0..9 {
+        let btn = ui.add(root, Button::new(&format!("Button {}", i)));
+        if i == 4 {
+            middle_btn = Some(btn);
+        }
+    }
+
+    let middle_btn = middle_btn.unwrap();
+
+    ui.asyncs.spawn(async move {
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        move |ui: &mut Ui| {
+            ui.set(middle_btn, |b: &mut Button| b.set_text("Changeddddddddddddddddddddddddddddd!"));
+        }
     });
 
     app.open_window(WindowConfig::default(), ui);
